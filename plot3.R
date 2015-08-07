c839a75ef9898dab085ce8fb3c6395642ac9179e
Mark Hummel <mdh@raquette.com>
2015-08-06 22:50:44 -0400

    plot3 working

---
+++ plot3.R
@@ -0,0 +1,53 @@
+## 
+## plot2.R
+##
+
+## declare filenames
+filename <- "household_power_consumption.txt"
+zipFilename <- "household_power_consumption.zip"
+
+## download file if needed
+if( !file.exists( zipFilename ) ) {
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile=zipFilename, method="curl")
+}
+
+## decompress file and turn into data frame
+data <- read.csv(unz(zipFilename,filename), header=TRUE, sep=";",stringsAsFactors=FALSE)
+
+## select subset of data to plot
+select1 <- as.Date(data$Date,format="%d/%m/%Y")>=as.Date("2007-02-01")
+select2 <- as.Date(data$Date,format="%d/%m/%Y")<=as.Date("2007-02-02")
+subData <- data[select1 & select2,]
+
+## plot data and convert to a png file
+
+png( file="plot3.png" )
+plot( 
+  strptime( paste(subdData$Time, subdData$Date), format="%H:%M:%S %d/%m/%Y"), 
+  as.numeric(subdData$Sub_metering_1), 
+  type='l',
+  col='black',
+  ylab="Energy sub metering", 
+  xlab=""
+)
+points( 
+  strptime( paste(subdData$Time, subdData$Date), format="%H:%M:%S %d/%m/%Y"), 
+  as.numeric(subdData$Sub_metering_2), 
+  type='l',
+  col='red'
+)
+points( 
+  strptime( paste(subdData$Time, subdData$Date), format="%H:%M:%S %d/%m/%Y"), 
+  as.numeric(subdData$Sub_metering_3), 
+  type='l',
+  col='blue'
+)
+legend( 
+  "topright", 
+  legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+  lty = 1,
+  col = c("black","red", "blue")
+)
+
+
+dev.off()
